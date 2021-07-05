16065f576596fd7de8004b16945d00cdae4a2737
cinyou <cinyou1999@163.com>
2021-07-05 17:00:04 +0800

    20210705

---
+++ Tree/Easy/101. 对称二叉树/Solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root == None:
+            return False
+        return self._dfs(root.left, root.right)
+    def _dfs(self, left, right):
+        if left == None and right == None:
+            return True
+        elif left == None or right == None:
+            return False
+        else:
+            return left.val == right.val and self._dfs(left.left, right.right) and self._dfs(left.right, right.left)
